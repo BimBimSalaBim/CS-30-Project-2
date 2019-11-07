37e208041f219c8a56ec9ff5d9aee9dad5524352
Faizan Zafar <faizanzafar619@gmail.com>
2019-11-07 13:31:51 -0800

    trade.cpp

---
+++ Project2-2/trade.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+vector<string> findPotentialBadTraders(vector<string> &s) {
+
+}
+
+
+int main() {
+	ifstream in("input.txt");
+	string str;
+	if (!in) {
+		cerr << "No input file" << endl;
+		exit (0);    
+	}    
+	vector<string> inputVec;
+	while (getline(in,str)) {
+		inputVec.push_back(str);
+	}   
+	vector<string> resV = findPotentialBadTraders(inputVec);
+	for (const string& r : resV) {
+		cout << r << endl;   
+	}   
+	return 0;
+}
