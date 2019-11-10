7f20902b8bfef35c49fcf79743c1c5004e9d61d8
Faizan Zafar <faizanzafar619@gmail.com>
2019-11-09 20:23:35 -0800

    finished trade.cpp

--- Project2-2/trade.cpp
+++ Project2-2/trade.cpp
@@ -5,53 +5,60 @@
 using namespace std;
 
 
-void ThreeDay(vector<vector<string>> &a, int d) {
+vector<string> ThreeDay( vector<vector<string>> &a) {
 	vector<vector<string>> days;
+	vector<string> results;
+
 	//put all the price changes into one vector for ease of access
 	for (int i = 0; i < a.size(); i++) {
 		if (a[i].size() == 2) {
 			days.push_back(a[i]);
 		}
 	}
+
+	//put all the trades into one vector for ease of access
 	vector<vector<string>> trades;
 	for (int i = 0; i < a.size(); i++) {
 		if (a[i].size() > 2) {
 			trades.push_back(a[i]);
 		}
 	}
+
 	//check if the stock was baught within 3 days of a price increase
 	for (int l = 0; l < days.size(); l++) {
 		for (int i = 0; i < trades.size(); i++) {
-			int daysBefore = stoi(trades[i][0]) - stoi(days[l][0]);
-			int daysAfter = stoi(days[l][0]) - stoi(trades[i][0]);
-			if (0 <= daysBefore && daysBefore <= 3 || 0 <= daysAfter && daysAfter <= 3) {
-				cout << trades[i][0] << "   ----   " << trades[i][1] << "   ----   " << days[l][0] << " ++++ " << daysAfter << " ++++ " << daysBefore << endl;
-				if (daysBefore > 0) {
-					//check the prev if it is a sell
+			int tradesAfter = stoi(days[l][0]) - stoi(trades[i][0]);
+			if (0 <= tradesAfter && tradesAfter <= 3) {
+				if (tradesAfter > 0) {
+
+					//check if the trade made goes over the thrushold
+					int sell = stoi(trades[i][3]) * (stoi(days[l - 1][1]) - stoi(days[l][1]));
+					int buy = stoi(trades[i][3]) * (stoi(days[l][1]) - stoi(days[l - 1][1]));
+
+					//check if profit gained or loss prevented is over the thrushold
+					if ((trades[i][2] == "BUY" && buy >= 500000) || (trades[i][2] == "SELL" && sell >= 500000) ){
+
+						// check if the trade is already in the list
+						bool isInList = false;
+						for (int r = 0; r < results.size(); r++) {
+							if (results[r] == trades[i][0]+"|"+trades[i][1]) {
+								isInList = true;
+								break;
+							}
+						}
+
+						// if not in list then add it to the list
+						if (!isInList) {
+							cout << trades[i][1] << " " << sell << endl;;
+							results.push_back(trades[i][0] + "|" + trades[i][1]);
+						}
+					}
 				}
 			}
 		}
 	}
 
-
-
-	//if (d == 3) {
-	//
-	//	// get all from past 3 and next 3
-
-	//}
-	//else if (d == 0) {
-	//	//get 0-3
-	//}
-
-
-	//for (int q = 0; q < a.size(); q++)
-	//{
-	//	for (int k = 0; k < a[q].size(); k++)
-	//	{
-	//		cout << a[q][k] << endl;
-	//	}
-	//}
+	return results;
 
 
 }
@@ -62,41 +69,37 @@ vector<string> findPotentialBadTraders(vector<string> &s) {
 	for (int i = 0; i < s.size(); i++) {
 		string line = s[i];
 		vector<string> word;
+		//End of Last
 		int EOL = 0;
 		int place = 0;
 		string splitter = "|";
 
-		// itterate through each char of the lin
+		// itterate through each char of the line
 		for (int j = 0; j < s[i].length(); j++) {
+
 			//check if the char at i is the same as the deliminator
 			if (string(1,line[j]) == splitter) {
+
 				// if it is then add it to vector and move the End of Last word to one more than what it is
 				word.push_back(line.substr(EOL,j-EOL));
 				EOL = j + 1;
 				place++;
 			}
+
 			//if there are no more deliminators and then add the last part of the line to the vector
 			if (place == 3 || (line.length() < 6 && place == 1)) {
 				word.push_back(line.substr(EOL));
 				place++;
 			}
 		}
+
 		//put the words vecto into the 2d vector of a 
 		a.push_back(word);
 		
 	}
 	
-	ThreeDay(a, 3);
 
-	//for (int q = 0; q < a.size(); q++)
-	//{
-	//	for (int k = 0; k < a[q].size(); k++)
-	//	{
-	//		cout << a[q][k] << endl;
-	//	}
-	//}
-
-	return s;
+	return ThreeDay(a);
 }
 
 
